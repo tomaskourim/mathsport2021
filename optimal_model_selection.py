8c4cb3c7180382ccc727982e8a1691448375afd6
Tomáš Kouřim <kourim@outlook.com>
2021-05-20 23:42:46 +0200

    model estimat v1

--- optimal_model_selection.py
+++ optimal_model_selection.py
@@ -70,6 +70,128 @@ def transform_data(matches_data: pd.DataFrame) -> Tuple[List[List[int]], List[fl
     return walks, starting_probabilities
 
 
+def get_current_probability(c_lambdas: List[float], last_probability: float, step: int, walk_type: str) -> float:
+    """
+    Computes the transition probability for the next step according to the respective definition as in the paper.
+    :param c_lambdas:
+    :param last_probability:
+    :param step: as Ising variable
+    :param walk_type:
+    :return:
+    """
+    if step == '' or step == 0:  # at the beginning of the walk just return p0
+        return last_probability
+    if walk_type == 'success_punished':
+        return c_lambdas[0] * last_probability + (0.5 * (1 - c_lambdas[0]) * (1 - step))
+    elif walk_type == 'success_rewarded':
+        return (c_lambdas[0]) * last_probability + (0.5 * (1 - c_lambdas[0]) * (1 + step))
+    elif walk_type == 'success_punished_two_lambdas':
+        return 0.5 * (((1 + step) * c_lambdas[0]) * last_probability + (1 - step) * (
+                1 - (c_lambdas[1]) * (1 - last_probability)))
+    elif walk_type == 'success_rewarded_two_lambdas':
+        return 0.5 * (((1 - step) * c_lambdas[0]) * last_probability + ((1 + step) * (
+                1 - (c_lambdas[1]) * (1 - last_probability))))
+    else:
+        raise Exception(f'Unexpected walk type: {walk_type}')
+
+
+def get_single_walk_log_likelihood(log_likelihood: float, c_lambdas: List[float], starting_probability: float,
+                                   walk: List[int], model_type: str, starting_index: int) -> float:
+    current_probability = starting_probability
+    for i in range(starting_index, len(walk)):
+        current_probability = get_current_probability(c_lambdas, current_probability, walk[i - 1], model_type)
+        if current_probability >= 1 or current_probability <= 0 or max(c_lambdas) >= 1 or min(c_lambdas) <= 0:
+            return -1e20  # so that I dont get double overflow error
+        log_likelihood = log_likelihood + np.log(
+            walk[i] * current_probability + (1 - walk[i]) * (1 - current_probability))
+    return log_likelihood
+
+
+def negative_log_likelihood_single_lambda(c_lambda: float, walks: List[List[int]], starting_probabilities: List[float],
+                                          model_type: str) -> float:
+    log_likelihood = 0
+    for starting_probability, walk in zip(starting_probabilities, walks):
+        log_likelihood = get_single_walk_log_likelihood(log_likelihood, [c_lambda], starting_probability, walk,
+                                                        model_type, starting_index=1)
+    return -log_likelihood
+
+
+def negative_log_likelihood_two_lambdas(c_lambdas: List[float], walks: List[List[int]],
+                                        starting_probabilities: List[float],
+                                        model_type: str) -> float:
+    log_likelihood = 0
+    for starting_probability, walk in zip(starting_probabilities, walks):
+        log_likelihood = get_single_walk_log_likelihood(log_likelihood, c_lambdas, starting_probability, walk,
+                                                        model_type, starting_index=1)
+    return -log_likelihood
+
+
+def find_akaike_single(model: str, walks: List[List[int]], starting_probabilities: List[float], result: List[float],
+                       current_model: str,
+                       min_akaike: float) -> Tuple[float, List[float], str]:
+    opt_result = opt.minimize_scalar(negative_log_likelihood_single_lambda, bounds=(0, 1), method='bounded',
+                                     args=(walks, starting_probabilities, model))
+    akaike = 2 + 2 * opt_result.fun
+    if not opt_result.success:
+        logger.error(f"Could not fit model {model}")
+    if opt_result.success and akaike < min_akaike:
+        min_akaike = akaike
+        current_model = model
+        result = opt_result.x
+    return min_akaike, result, current_model
+
+
+def find_akaike(guess: np.ndarray, model: str, walks: List[List[int]], starting_probabilities: List[float],
+                result: List[float], current_model: str, min_akaike: float) -> Tuple[float, List[float], str]:
+    opt_result = opt.minimize(negative_log_likelihood_two_lambdas, guess, method=OPTIMIZATION_ALGORITHM,
+                              args=(walks, starting_probabilities, model))
+    akaike = 2 * len(guess) + 2 * opt_result.fun
+    if not opt_result.success:
+        logger.error(f"Could not fit model {model}")
+    if opt_result.success and akaike < min_akaike:
+        min_akaike = akaike
+        current_model = model
+        result = opt_result.x
+    return min_akaike, result, current_model
+
+
+def get_model_estimate(walks: List[List[int]], starting_probabilities: List[float]) -> Tuple[List[float], str]:
+    """
+    Uses the Akaike information criterion
+    https://en.wikipedia.org/wiki/Akaike_information_criterion#Modification_for_small_sample_size
+    to get the optimal model.
+    AIC = 2k - 2ln(L)
+    opt.minimize() returns directly -ln(L)
+    :param walks:
+    :param starting_probabilities:
+    :return:
+    """
+    result = [ERROR_VALUE, ERROR_VALUE]
+    current_model = ERROR_VALUE
+    min_akaike = 1e20  # big enough
+
+    # single lambda models
+    model = 'success_punished'
+    min_akaike, result, current_model = find_akaike_single(model, walks, starting_probabilities, result, current_model,
+                                                           min_akaike)
+
+    model = 'success_rewarded'
+    min_akaike, result, current_model = find_akaike_single(model, walks, starting_probabilities, result, current_model,
+                                                           min_akaike)
+
+    # two lambdas models
+    guess = np.repeat(0.5, 2)
+    model = 'success_punished_two_lambdas'
+    min_akaike, result, current_model = find_akaike(guess, model, walks, starting_probabilities, result, current_model,
+                                                    min_akaike)
+
+    model = 'success_rewarded_two_lambdas'
+    min_akaike, result, current_model = find_akaike(guess, model, walks, starting_probabilities, result, current_model,
+                                                    min_akaike)
+
+    return result, current_model
+
+
 def main():
     # get all data
     start_date = '2021-02-01 00:00:00.000000'
@@ -81,6 +203,8 @@ def main():
     logger.info(f"There are {len(walks)} walks available.")
 
     # get model estimate + parameters
+    c_lambdas, model_type = get_model_estimate(walks, starting_probabilities)
+
     # repeat for subgroups
     logger.info("Done")
 
